e1e984f02ee73af0b41323c2fbdfe8751f2597fd
sandalconsumer <sandalconsumer@hotmail.com>
2025-06-04 23:12:22 +0300

    Dedicated Harmonic Visualization Script for Modularity
    separated visualization script from main body script for better modularity

--- Assets/Scripts/Physics/Body.cs
+++ Assets/Scripts/Physics/Body.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 using System.Collections.Generic;
 
 [System.Serializable]
-public class Spring // in this simulation, springs are considered to all be acting on one body, and class "Body" is a singleton although not explicitly being defined as that.
+public class Spring 
 {
     public Vector3 rootPosition = new Vector3(0,0,0);
     public float springConstant = 100;
 
     [HideInInspector]public Vector3 restorativeForce = Vector3.zero;
-    [HideInInspector]public Vector3 stretch; //how far the mass is from the root position
+    [HideInInspector]public Vector3 stretch;
     public Vector3 restPosition = new Vector3(3, 0, 0);
 }
 
 public class Body : MonoBehaviour
 {
+    public SimulationDataCompiler dataCompiler;
+    
     Vector3 position;
     Vector3 acceleration;
     Vector3 velocity;
@@ -25,13 +27,6 @@ public class Body : MonoBehaviour
     public List<Spring> connectedSprings = new List<Spring>();
 
     private float timestep;
-    
-    public float plotScale = 1f;
-    public Vector3 plotOrigin = new Vector3(0, 0, -10);
-    
-    private List<float> timeStamps = new List<float>();
-    private List<Vector3> positions = new List<Vector3>();
-    private List<float> stretches = new List<float>();
     
     void Start()
     {
@@ -64,16 +59,10 @@ public class Body : MonoBehaviour
         
         VerletFinalstep(timestep);
         
-        timeStamps.Add(Time.fixedTime);
-        positions.Add(position);
-        stretches.Add(connectedSprings[0].stretch.magnitude);
+        dataCompiler.simulationEndData.timeStamps.Add(Time.fixedTime);
+        dataCompiler.simulationEndData.positions.Add(position);
+        dataCompiler.simulationEndData.stretches.Add(connectedSprings[0].stretch.magnitude);
         
-        for (int i = 1; i < timeStamps.Count; i++)
-        {
-            Vector3 p1 = plotOrigin + new Vector3(timeStamps[i-1] * plotScale, stretches[i-1] * plotScale, 0);
-            Vector3 p2 = plotOrigin + new Vector3(timeStamps[i] * plotScale, stretches[i] * plotScale, 0);
-            Debug.DrawLine(p1, p2, Color.cyan);
-        }
     }
 
     void UpdateSpringDisplacement(List<Spring> springs)
---
+++ Assets/Scripts/Utilities/SimulationDataCompiler.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SimulationEndData
+{
+    public List<float> timeStamps = new List<float>();
+    public List<Vector3> positions = new List<Vector3>();
+    public List<float> stretches = new List<float>();
+}
+
+public class SimulationDataCompiler : MonoBehaviour // to be used as a singleton
+{
+    public SimulationEndData simulationEndData = new SimulationEndData();
+    
+    public float plotScale = 1f;
+    public Vector3 plotOrigin = new Vector3(10, 0, 0);
+    
+    void Update()
+    {
+        for (int i = 1; i < simulationEndData.timeStamps.Count; i++)
+        {
+            Vector3 p1 = plotOrigin + new Vector3(simulationEndData.timeStamps[i-1] * plotScale, simulationEndData.stretches[i-1] * plotScale, 0);
+            Vector3 p2 = plotOrigin + new Vector3(simulationEndData.timeStamps[i] * plotScale, simulationEndData.stretches[i] * plotScale, 0);
+            Debug.DrawLine(p1, p2, Color.cyan);
+        }
+    }
+}
